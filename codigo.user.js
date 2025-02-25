2d69b8cbaebf23b28ff448e678457e6b014d8303
barril <74427600+UmBarril@users.noreply.github.com>
2025-02-25 09:48:40 -0300

    Update codigo.user.js
    modificando código para funcionar com o sigaa da ufpb

--- codigo.user.js
+++ codigo.user.js
@@ -1,17 +1,18 @@
 // ==UserScript==
 // @name         Consertando os horários do SIGAA UnB
-// @namespace    https://github.com/luthierycosta
-// @version      1.2.5
-// @icon         https://github.com/luthierycosta/ConsertandoHorariosSIGAA/blob/master/images/icon.png?raw=true
-// @description  Traduz as informações de horários das turmas no SIGAA (novo sistema da UnB), de formato pouco entendível, por dias e horas escritas por extenso.
-// @author       Luthiery Costa
-// @supportURL   https://github.com/luthierycosta
+// @namespace    https://github.com/UmBarril
+// @version      1.2.6
+// @icon         https://github.com/UmBarril/ConsertandoHorariosSIGAA/blob/master/images/icon.png?raw=true
+// @description  Traduz as informações de horários das turmas no SIGAA da UFPB, de formato pouco entendível, por dias e horas escritas por extenso.
+// @author       João Barros
 // @match        https://sigaa.unb.br/sigaa/*
 // @grant        none
 // @noframes
 // ==/UserScript==
 
 'use strict';
+
+// TODO AUMENTAR TAMANHO DA COLUNA DE HORARIOS
 
 /** Cria dicionário para mapear os números e letras aos dias e horas reais */
 const mapaDias = {
@@ -23,22 +24,21 @@ const mapaDias = {
     7: 'SAB'
 }
 const mapaHorarios = {
-    'M1': {inicio: '08:00', fim: '08:55'},
-    'M2': {inicio: '08:55', fim: '09:50'},
-    'M3': {inicio: '10:00', fim: '10:55'},
-    'M4': {inicio: '10:55', fim: '11:50'},
-    'M5': {inicio: '12:00', fim: '12:55'},
-    'T1': {inicio: '12:55', fim: '13:50'},
-    'T2': {inicio: '14:00', fim: '14:55'},
-    'T3': {inicio: '14:55', fim: '15:50'},
-    'T4': {inicio: '16:00', fim: '16:55'},
-    'T5': {inicio: '16:55', fim: '17:50'},
-    'T6': {inicio: '18:00', fim: '18:55'},
-    'T7': {inicio: '18:55', fim: '19:50'},
-    'N1': {inicio: '19:00', fim: '19:50'},
-    'N2': {inicio: '19:50', fim: '20:40'},
-    'N3': {inicio: '20:50', fim: '21:40'},
-    'N4': {inicio: '21:40', fim: '22:30'}
+    'M2': {inicio: '08:00', fim: '9:00'},
+    'M3': {inicio: '10:00', fim: '10:00'},
+    'M4': {inicio: '10:00', fim: '11:00'},
+    'M5': {inicio: '12:00', fim: '12:00'},
+    'T1': {inicio: '13:00', fim: '14:00'},
+    'T2': {inicio: '14:00', fim: '15:00'},
+    'T3': {inicio: '15:00', fim: '16:00'},
+    'T4': {inicio: '16:00', fim: '17:00'},
+    'T5': {inicio: '17:00', fim: '18:00'},
+    'T6': {inicio: '18:00', fim: '19:00'},
+    'T7': {inicio: '19:00', fim: '20:00'},
+    'N1': {inicio: '20:00', fim: '21:00'},
+    'N2': {inicio: '21:00', fim: '22:00'},
+    'N3': {inicio: '22:00', fim: '23:00'},
+    'N4': {inicio: '23:00', fim: '00:00'}
 }
 
 /** Padrão regex que reconhece o formato de horário do SIGAA */
@@ -53,18 +53,24 @@ const padraoSigaa = /\b([2-7]{1,5})([MTN])([1-7]{1,7})\b/gm;
  * @param {*} g3        O terceiro grupo de captura do regex - no caso, o conjunto de dígitos dos horários
  */
 function mapeiaHorarios(match, g1, g2, g3) {
+  try {
     let dia         = mapaDias[g1];
     let hora_inicio = mapaHorarios[`${g2}${g3.charAt(0)}`].inicio;
     let hora_fim    = mapaHorarios[`${g2}${g3.charAt(g3.length-1)}`].fim;
     return `${dia} ${hora_inicio}-${hora_fim}`;
+  } catch(e) {
+    console.log(e)
+    return `${g1}${g2}${g3}`;
+  }
+  return `${dia} ${hora_inicio}-${hora_fim}`;
 }
 
 /**
  * Função que separa os dias para que toda "palavra" de horário tenha só 1 dígito de dia antes do turno.
  * ex: 246M12 vira 2M12 4M12 6M12.
- * 
+ *
  * Quando já está devidamente separado, retorna o mesmo texto.
- * 
+ *
  * @param {*} match     O horário completo reconhecido pelo regex
  * @param {*} g1        O primeiro grupo de captura do regex - no caso, o(s) dígito(s) do dia da semana
  * @param {*} g2        O segundo grupo de captura do regex - no caso, a letra do turno
@@ -74,10 +80,10 @@ function separaDias(match, g1, g2, g3) {
     return [...g1].map(dia => `${dia}${g2}${g3}`).join(' ');
 }
 
-/** 
+/**
  * Função que recebe o texto com os horários e o ordena pela ordem dos dias da semana
  * Ou seja, o primeiro dígito de cada "palavra".
- * 
+ *
  * @param {*} texto     O texto HTML dos horários separados por espaço, que já foi tratado pela separaDias().
  * @returns   O texto com os horários ordenados separados por espaço.
  */
@@ -102,10 +108,11 @@ const treeWalker = document.createTreeWalker(
 /** Procura por todos os textos e, onde reconhecer o padrão de horário, executa a substituição */
 let node;
 while(node = treeWalker.nextNode()){
+    console.log(node.textContent)
     node.textContent = node.textContent.replace(padraoSigaa,separaDias);
     node.textContent = ordenaDias(node.textContent);
     node.textContent = node.textContent.replace(padraoSigaa,mapeiaHorarios);
-    
+
     // por fim, junta as ocorrências 12:00-12:55/12:55-13:50 em simplesmente 12:00-13:50
     node.textContent = node.textContent.replace(/([A-Z]{3}) 12:00-12:55 ([A-Z]{3}) 12:55-13:50/gm, '$1 12:00-13:50')
 }
